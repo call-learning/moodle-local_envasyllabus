bdf6db0738bf727f0021fe1ecd5f04c87ce0a491
Laurent David <lmedavid@gmail.com>
2026-01-06 17:57:24 +0100

    Update version

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_envasyllabus';
-$plugin->release = '2.2.1';
-$plugin->version = 2025122401;
+$plugin->release = '2.2.2';
+$plugin->version = 2026010500;
 $plugin->requires = 2020061500;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
